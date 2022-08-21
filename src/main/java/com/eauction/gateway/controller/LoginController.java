b443c874be51869d6e6fed2a80e613ec746669c7
Prasad <prasadrpm@hotmail.com>
2022-08-21 11:50:18 +0530

    crossorigin changes

--- src/main/java/com/eauction/gateway/controller/LoginController.java
+++ src/main/java/com/eauction/gateway/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.eauction.gateway.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import com.google.common.base.Optional;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin
 public class LoginController {
 	
 	@Autowired
